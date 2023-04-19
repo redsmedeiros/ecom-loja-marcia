cc68756f1ff932dc249fddc5494c3312d3aeb517
Rodolpho <redsmedeiros@gmail.com>
2023-04-19 12:22:01 -0300

    primeiro

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from 'http';
+import app from './app/app.js';
+
+
+const PORT = process.env.PORT || 7000;
+const server = http.createServer(app);
+server.listen(PORT, console.log(`Server is runnin at ${PORT}`));
+
